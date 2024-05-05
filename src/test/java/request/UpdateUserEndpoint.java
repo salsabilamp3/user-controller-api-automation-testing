a43d3ff7e3cc814b8f13916ad0aba5f09f1510cc
salsabilamp3 <salsabilamp3@gmail.com>
2024-05-05 11:27:30 +0700

    add the rest of update tests

--- src/test/java/request/UpdateUserEndpoint.java
+++ src/test/java/request/UpdateUserEndpoint.java
@@ -23,6 +23,33 @@ public class UpdateUserEndpoint {
         return request.put(EndPoint.UPDATE_USER + userId);
     }
 
+    public static User prepareTestUpdateAll() {
+        User dataUser = new User();
+
+        dataUser.setId("60d0fe4f5311236168a10a04");
+        dataUser.setTitle("ms");
+        dataUser.setFirstName("Titi");
+        dataUser.setLastName("Jane");
+        dataUser.setPicture("https://example.com/profile_picture_tati.jpg");
+        dataUser.setGender("female");
+        dataUser.setEmail("konsta.manninen@example.com");
+        dataUser.setDateOfBirth("1995-08-15");
+        dataUser.setPhone("+62817287187");
+
+        Location location = new Location();
+        location.setStreet("123 Oak Street");
+        location.setCity("Los Angeles");
+        location.setState("California");
+        location.setCountry("United States");
+        location.setTimezone("-08:00");
+        dataUser.setLocation(location);
+
+        dataUser.setRegisterDate("2021-06-21T21:02:17.280Z");
+        dataUser.setUpdatedDate("2021-06-21T21:02:17.280Z");
+
+        return dataUser;
+    }
+
     public static User prepareTestUpdateTitle() {
         User dataUser = new User();
 
@@ -35,14 +62,44 @@ public class UpdateUserEndpoint {
         dataUser.setEmail("clea.dubois@example.com");
         dataUser.setDateOfBirth("1976-12-19T09:54:30.283Z");
         dataUser.setPhone("05-07-68-29-63");
-        dataUser.setLocation(new Location());
-        dataUser.getLocation().setStreet("5348, Rue Barrier");
-        dataUser.getLocation().setCity("Perpignan");
-        dataUser.getLocation().setState("Haut-Rhin");
-        dataUser.getLocation().setCountry("France");
-        dataUser.getLocation().setTimezone("-11:00");
+
+        Location location = new Location();
+        location.setStreet("5348, Rue Barrier");
+        location.setCity("Perpignan");
+        location.setState("Haut-Rhin");
+        location.setCountry("France");
+        location.setTimezone("-11:00");
+        dataUser.setLocation(location);
+
         dataUser.setRegisterDate("2021-06-21T21:02:18.060Z");
         dataUser.setUpdatedDate("2021-06-21T21:02:18.060Z");
+
+        return dataUser;
+    }
+
+    public static User prepareTestUpdateGender() {
+        User dataUser = new User();
+
+        dataUser.setId("60d0fe4f5311236168a10a18");
+        dataUser.setTitle("mr");
+        dataUser.setFirstName("Jeremy");
+        dataUser.setLastName("Morin");
+        dataUser.setPicture("https://randomuser.me/api/portraits/med/men/60.jpg");
+        dataUser.setGender("");
+        dataUser.setEmail("jeremy.morin@example.com");
+        dataUser.setDateOfBirth("1951-02-21T09:25:59.841Z");
+        dataUser.setPhone("081-702-1111");
+
+        Location location = new Location();
+        location.setStreet("1693, Argyle St");
+        location.setCity("Flatrock");
+        location.setState("Alberta");
+        location.setCountry("Canada");
+        location.setTimezone("-4:00");
+        dataUser.setLocation(location);
+
+        dataUser.setRegisterDate("2021-06-21T21:02:20.521Z");
+        dataUser.setUpdatedDate("2021-06-21T21:02:20.521Z");
 
         return dataUser;
     }
--- src/test/java/testapi/UpdateTest.java
+++ src/test/java/testapi/UpdateTest.java
@@ -6,10 +6,65 @@ import org.junit.jupiter.api.Test;
 
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
+import model.Location;
 import model.User;
 import request.UpdateUserEndpoint;
 
 public class UpdateTest {
+    // Eksekusi TC-3-4
+    @Test
+    public void testUpdateAll() {
+        // menyiapkan data test
+        String userId = "60d0fe4f5311236168a10a04";
+        User testData = new User();
+
+        testData.setTitle("ms");
+        testData.setFirstName("Titi");
+        testData.setLastName("Jane");
+        testData.setGender("female");
+        testData.setDateOfBirth("1995-08-15");
+        testData.setPhone("+62817287187");
+        testData.setPicture("https://example.com/profile_picture_tati.jpg");
+
+        Location location = new Location();
+        location.setStreet("123 Oak Street");
+        location.setCity("Los Angeles");
+        location.setState("California");
+        location.setCountry("United States");
+        location.setTimezone("-08:00");
+        testData.setLocation(location);
+
+        Response response = UpdateUserEndpoint.updateAllField(userId, testData);
+        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("user-schema.json"));
+
+        // periksa actual status code
+        assertEquals(200, response.getStatusCode());
+
+        // periksa actual response body
+        User expectedBody = UpdateUserEndpoint.prepareTestUpdateAll();
+        User actualBody = response.as(User.class);
+
+        assertEquals(expectedBody.getId(), actualBody.getId());
+        assertEquals(expectedBody.getTitle(), actualBody.getTitle());
+        assertEquals(expectedBody.getFirstName(), actualBody.getFirstName());
+        assertEquals(expectedBody.getLastName(), actualBody.getLastName());
+        assertEquals(expectedBody.getPicture(), actualBody.getPicture());
+        assertEquals(expectedBody.getGender(), actualBody.getGender());
+        assertEquals(expectedBody.getEmail(), actualBody.getEmail());
+        assertEquals(expectedBody.getDateOfBirth(), actualBody.getDateOfBirth());
+        assertEquals(expectedBody.getPhone(), actualBody.getPhone());
+
+        assertEquals(expectedBody.getLocation().getStreet(), actualBody.getLocation().getStreet());
+        assertEquals(expectedBody.getLocation().getCity(), actualBody.getLocation().getCity());
+        assertEquals(expectedBody.getLocation().getState(), actualBody.getLocation().getState());
+        assertEquals(expectedBody.getLocation().getCountry(), actualBody.getLocation().getCountry());
+        assertEquals(expectedBody.getLocation().getTimezone(), actualBody.getLocation().getTimezone());
+
+        assertEquals(expectedBody.getRegisterDate(), actualBody.getRegisterDate());
+        assertEquals(expectedBody.getUpdatedDate(), actualBody.getUpdatedDate());
+    }
+
+    // Eksekusi TC-3-9
     @Test
     public void testUpdateTitle() {
         // menyiapkan data test
@@ -18,7 +73,7 @@ public class UpdateTest {
         String value = "mr";
 
         Response response = UpdateUserEndpoint.updateOneField(userId, fieldName, value);
-        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("user-schema.json"));//
+        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("user-schema.json"));
 
         // periksa actual status code
         assertEquals(200, response.getStatusCode());
@@ -45,5 +100,83 @@ public class UpdateTest {
 
         assertEquals(expectedBody.getRegisterDate(), actualBody.getRegisterDate());
         assertEquals(expectedBody.getUpdatedDate(), actualBody.getUpdatedDate());
+    }
+
+    // Eksekusi TC-3-18
+    @Test
+    public void testUpdateGender() {
+        // menyiapkan data test
+        String userId = "60d0fe4f5311236168a10a18";
+        String fieldName = "gender";
+        String value = "";
+
+        Response response = UpdateUserEndpoint.updateOneField(userId, fieldName, value);
+        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("user-schema.json"));
+
+        // periksa actual status code
+        assertEquals(200, response.getStatusCode());
+
+        // periksa actual response body
+        User expectedBody = UpdateUserEndpoint.prepareTestUpdateGender();
+        User actualBody = response.as(User.class);
+
+        assertEquals(expectedBody.getId(), actualBody.getId());
+        assertEquals(expectedBody.getTitle(), actualBody.getTitle());
+        assertEquals(expectedBody.getFirstName(), actualBody.getFirstName());
+        assertEquals(expectedBody.getLastName(), actualBody.getLastName());
+        assertEquals(expectedBody.getPicture(), actualBody.getPicture());
+        assertEquals(expectedBody.getGender(), actualBody.getGender());
+        assertEquals(expectedBody.getEmail(), actualBody.getEmail());
+        assertEquals(expectedBody.getDateOfBirth(), actualBody.getDateOfBirth());
+        assertEquals(expectedBody.getPhone(), actualBody.getPhone());
+
+        assertEquals(expectedBody.getLocation().getStreet(), actualBody.getLocation().getStreet());
+        assertEquals(expectedBody.getLocation().getCity(), actualBody.getLocation().getCity());
+        assertEquals(expectedBody.getLocation().getState(), actualBody.getLocation().getState());
+        assertEquals(expectedBody.getLocation().getCountry(), actualBody.getLocation().getCountry());
+        assertEquals(expectedBody.getLocation().getTimezone(), actualBody.getLocation().getTimezone());
+
+        assertEquals(expectedBody.getRegisterDate(), actualBody.getRegisterDate());
+        assertEquals(expectedBody.getUpdatedDate(), actualBody.getUpdatedDate());
+    }
+
+    // Eksekusi TC-3-27
+    @Test
+    public void testUpdateEmail() {
+        // menyiapkan data test
+        String userId = "60d0fe4f5311236168a10a27";
+        String fieldName = "email";
+        String value = "emails@example.com";
+
+        Response response = UpdateUserEndpoint.updateOneField(userId, fieldName, value);
+
+        // periksa actual status code
+        assertEquals(400, response.getStatusCode());
+
+        // periksa actual response body
+        String expectedResult = "BODY_NOT_VALID";
+        String actualResult = response.getBody().jsonPath().getString("error");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    // Eksekusi TC-3-55
+    @Test
+    public void testUpdateNotExistedId() {
+        // menyiapkan data test
+        String userId = "60d0fe4f5311236168a10a35";
+        User testData = new User();
+        testData.setTitle("mr");
+        testData.setFirstName("Tomothy");
+        testData.setLastName("Hawkins");
+
+        Response response = UpdateUserEndpoint.updateAllField(userId, testData);
+
+        // periksa actual status code
+        assertEquals(404, response.getStatusCode());
+
+        // periksa actual response body
+        String expectedResult = "RESOURCE_NOT_FOUND";
+        String actualResult = response.getBody().jsonPath().getString("error");
+        assertEquals(expectedResult, actualResult);
     }
 }

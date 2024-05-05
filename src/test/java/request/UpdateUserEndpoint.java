350fbaf4b08a0ad3ce81e25009a9c62f74761aa5
salsabilamp3 <salsabilamp3@gmail.com>
2024-05-05 09:32:41 +0700

    add endpoint header

--- src/test/java/request/UpdateUserEndpoint.java
+++ src/test/java/request/UpdateUserEndpoint.java
@@ -8,6 +8,7 @@ import model.User;
 public class UpdateUserEndpoint {
     public static Response updateOneField(String userId, String fieldName, String fieldValue) {
         RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
         request.header("app-id", "66273c13e26079618814ec13");
         request.body("{\"" + fieldName + "\":\"" + fieldValue + "\"}");
         return request.put(EndPoint.UPDATE_USER + userId);
@@ -15,6 +16,7 @@ public class UpdateUserEndpoint {
 
     public static Response updateAllField(String userId, User user) {
         RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
         request.header("app-id", "66273c13e26079618814ec13");
         request.body(user);
         return request.put(EndPoint.UPDATE_USER + userId);
